2cf8856449906d779aa026ca7f7dbcb92f274f02
chuhx <chuhx1024@126.com>
2022-01-11 16:13:56 +0800

    feat: login API done

---
+++ src/api/user.ts
@@ -0,0 +1,29 @@
+import { request } from '@/utils/request'
+
+interface LoginInput {
+    email: string,
+    password: string,
+
+}
+interface User {
+    email: string,
+    password: string,
+    username: string,
+    channelDescription?: string,
+    avatar?: string
+}
+
+interface LoginPayload {
+ user: User
+}
+
+export const login = (data: LoginInput) => {
+    return request.post<LoginPayload>('/api/v1/users/login', data)
+}
+
+// login({
+//     email: '123',
+//     password: '44',
+// }).then(res => {
+//     res.data.user.email.slice
+// })
