20a242cca1ec872f847460026a22e2d69ffcdad0
chuhx <chuhx1024@126.com>
2022-01-11 15:54:29 +0800

    feat: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/standard',
+        '@vue/typescript/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 1 : 0,
+        indent: [2, 4], // js 4个空格缩进
+        'vue/html-indent': [2, 4], // html 4个空格缩进
+        eqeqeq: 0, // 允许使用 == !=
+        'comma-dangle': [2, 'always-multiline'], // 对象 数据 分行显示的 结尾必须加 ','
+        'quote-props': 0,
+    },
 }
