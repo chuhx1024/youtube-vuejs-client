6cfd2c0844523705340931505c31f60be81ec9ed
chuhx <chuhx1024@126.com>
2022-01-12 11:30:38 +0800

    fix: env bug

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const request = axios.create({
-    // baseURL: process.env.VUE_APP_BASE_URL,
-    baseURL: 'http://127.0.0.1:7002/',
+    baseURL: process.env.VUE_APP_API_BASE_URL,
 })
